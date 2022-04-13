24e477c89e7dbf501d291197ab3f58fd99ae1466
97kimdong <kjhtty@naver.com>
2022-04-13 17:34:31 +0900

    220413수업

---
+++ Ex/src/Confirm/ChildEx.java
@@ -0,0 +1,12 @@
+package Confirm;
+
+public class ChildEx {
+	public static void main(String[] args) {
+		Child child1 = new Child("김기태", 6);
+
+		Child child2 = new Child("김기태", 7);
+		
+		Child child3 = new Child("김기태", 9);
+		
+	}
+}
